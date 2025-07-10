4580ea43ed0a49aad493ada6b53ebae81144c623
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:50:52 +0300

    updated hive services

--- lib/data/datasources/local/hive_service.dart
+++ lib/data/datasources/local/hive_service.dart
@@ -1,1 +1,12 @@
-﻿// 
+﻿import 'package:hive/hive.dart';
+
+class HiveService {
+  static Future<void> init() async {
+    await Hive.openBox('expenses');
+    await Hive.openBox('budgets');
+  }
+
+  static Box getExpenseBox() {
+    return Hive.box('expenses');
+  }
+}
