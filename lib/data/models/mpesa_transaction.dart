f322d45e8ac708d36e63e8955f9f3cb59e9b48f2
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:50:04 +0300

    updated mpesa transactions

--- lib/data/models/mpesa_transaction.dart
+++ lib/data/models/mpesa_transaction.dart
@@ -1,1 +1,15 @@
-﻿// 
+﻿class MpesaTransaction {
+  final String transactionId;
+  final double amount;
+  final DateTime date;
+  final String recipient;
+  final String reference;
+
+  MpesaTransaction({
+    required this.transactionId,
+    required this.amount,
+    required this.date,
+    required this.recipient,
+    required this.reference,
+  });
+}
