957f32b47ebde8bff280cb4d2ab454e4d33ac019
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 17:16:28 +0300

    updated the root file

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,1 +1,25 @@
-﻿
+﻿import 'package:flutter/material.dart';
+import 'package:pesa_planner/core/theme/app_theme.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pesa Planner',
+      theme: kenyanTheme,
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Login Screen')));
+  }
+}
