fa236aab4235667b168aabafdfa5ca54b6339f06
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:45:50 +0300

    updated expense model

--- lib/data/models/expense_model.dart
+++ lib/data/models/expense_model.dart
@@ -1,1 +1,34 @@
-﻿// 
+﻿class Expense {
+  final String id;
+  final String category;
+  final double amount;
+  final DateTime date;
+  final String description;
+  final bool isRecurring;
+
+  Expense({
+    required this.id,
+    required this.category,
+    required this.amount,
+    required this.date,
+    this.description = '',
+    this.isRecurring = false,
+  });
+
+  static const List<String> kenyanCategories = [
+    'Transport',
+    'Utilities',
+    'M-PESA',
+    'Food',
+    'Airtime',
+    'Chama',
+    'Entertainment',
+    'Healthcare',
+  ];
+}
+
+extension ExpenseCategoryExtension on String {
+  bool get isValidKenyanCategory {
+    return Expense.kenyanCategories.contains(this);
+  }
+}
