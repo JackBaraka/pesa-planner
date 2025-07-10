5da5b9c59061518fdd8e355ba35e2f1792cd3caf
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:26:14 +0300

    updated firebase constants

--- lib/core/constants/firebase_constants.dart
+++ lib/core/constants/firebase_constants.dart
@@ -1,1 +1,5 @@
-﻿// 
+﻿class FirebaseConstants {
+  static const String usersCollection = "users";
+  static const String budgetsCollection = "budgets";
+  static const String expensesCollection = "expenses";
+}
