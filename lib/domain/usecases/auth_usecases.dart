4eeb65d6eef9b8f910efd803406cede3029a095f
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 17:06:14 +0300

    updated authenticator usecases

--- lib/domain/usecases/auth_usecases.dart
+++ lib/domain/usecases/auth_usecases.dart
@@ -1,1 +1,9 @@
-﻿// 
+﻿import 'package:pesa_planner/domain/repositories/auth_repository_contract.dart';
+
+class AuthUseCases {
+  final AuthRepositoryContract repository;
+
+  AuthUseCases(this.repository);
+
+  Stream<AppUser> get currentUser => repository.user;
+}
