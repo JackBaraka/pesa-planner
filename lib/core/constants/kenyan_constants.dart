a7267212df91a8f262ce3a0c528ef15ce73f7f0a
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:23:49 +0300

    updated kenyan constants

--- lib/core/constants/kenyan_constants.dart
+++ lib/core/constants/kenyan_constants.dart
@@ -1,1 +1,21 @@
-﻿// 
+﻿class KenyanConstants {
+  static const List<String> counties = [
+    "Nairobi",
+    "Mombasa",
+    "Kisumu",
+    "Nakuru",
+    "Eldoret",
+  ];
+
+  static const Map<String, double> matatuFares = {
+    "CBD to Westlands": 100.0,
+    "Thika to Nairobi": 150.0,
+    "Ngong to CBD": 80.0,
+  };
+
+  static const List<String> utilityProviders = [
+    "KPLC",
+    "Nairobi Water",
+    "KETRACO",
+  ];
+}
