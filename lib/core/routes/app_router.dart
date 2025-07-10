1e18d1d946c1e2ef9659ae27404c27e280704b07
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:33:04 +0300

    updated app text styles

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,5 +1,6 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:pesa_planner/features/auth/presentation/screens/login_screen.dart';
+// Make sure that the LoginScreen class is defined as 'class LoginScreen extends StatelessWidget' and exported in the imported file.
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -10,6 +11,7 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (_) => Scaffold(
             body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
         );
     }
   }
--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -1,1 +1,14 @@
-﻿// 
+﻿import 'package:flutter/material.dart';
+
+TextStyle heading1 = const TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
+
+TextStyle bodyText = const TextStyle(fontSize: 16, color: Colors.black87);
+TextStyle buttonText = const TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: Colors.white,
+);
