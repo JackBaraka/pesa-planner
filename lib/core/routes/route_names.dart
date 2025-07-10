5debce517e3e3ecff6bc1286997d45c29299cc77
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:29:07 +0300

    updated route names

--- lib/core/routes/route_names.dart
+++ lib/core/routes/route_names.dart
@@ -1,1 +1,6 @@
-﻿// 
+﻿class RouteNames {
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String budget = '/budget';
+  static const String expenses = '/expenses';
+}
