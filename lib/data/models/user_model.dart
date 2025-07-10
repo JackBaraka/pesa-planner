5da50d911e379954067bcf4b1f685060a9db3376
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:49:15 +0300

    updated user model

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,1 +1,20 @@
-﻿// 
+﻿class AppUser {
+  final String uid;
+  final String? email;
+  final String? phone;
+  final String displayName;
+
+  AppUser({
+    required this.uid,
+    this.email,
+    this.phone,
+    required this.displayName,
+  });
+}
+
+extension AppUserExtension on AppUser {
+  bool get isEmailVerified => email != null && email!.isNotEmpty;
+
+  String get displayNameOrEmail =>
+      displayName.isNotEmpty ? displayName : (email ?? 'No Email');
+}
