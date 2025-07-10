0462482a27696fd18624e8dcf755898fb2156302
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:51:29 +0300

    added placeholder for local data source

--- lib/data/datasources/local/local_datasource.dart
+++ lib/data/datasources/local/local_datasource.dart
@@ -1,1 +1,5 @@
-﻿// 
+﻿// Placeholder for local data operations
+abstract class LocalDataSource {
+  Future<void> saveExpense(Map<String, dynamic> expense);
+  Future<List<Map<String, dynamic>>> getExpenses();
+}
