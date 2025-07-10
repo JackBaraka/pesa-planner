47194984d266f0ebfaebcf3fdbdec9c0b5e3763e
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:45:04 +0300

    updated budget model

--- lib/data/models/budget_model.dart
+++ lib/data/models/budget_model.dart
@@ -1,1 +1,17 @@
-﻿// 
+﻿class Budget {
+  final String id;
+  final String name;
+  final double amount;
+  final String category;
+  final DateTime startDate;
+  final DateTime endDate;
+
+  Budget({
+    required this.id,
+    required this.name,
+    required this.amount,
+    required this.category,
+    required this.startDate,
+    required this.endDate,
+  });
+}
