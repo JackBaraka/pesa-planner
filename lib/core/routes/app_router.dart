20869e0ae814623ae2c722b2fca2c439252abc37
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:28:23 +0300

    updated app router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,1 +1,16 @@
-﻿// 
+﻿import 'package:flutter/material.dart';
+import 'package:pesa_planner/features/auth/presentation/screens/login_screen.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+        );
+    }
+  }
+}
