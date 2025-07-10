62a838770b36726048eb916665b93fa8280eccc3
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:34:03 +0300

    updated input decoration

--- lib/core/theme/input_decoration.dart
+++ lib/core/theme/input_decoration.dart
@@ -1,1 +1,17 @@
-﻿// 
+﻿import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+InputDecorationTheme kenyanInputTheme() {
+  return InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: const BorderSide(color: AppColors.kenyaGreen),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: const BorderSide(color: AppColors.kenyaGold, width: 2),
+    ),
+    labelStyle: const TextStyle(color: AppColors.kenyaGreen),
+    floatingLabelStyle: const TextStyle(color: AppColors.kenyaGold),
+  );
+}
