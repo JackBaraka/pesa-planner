bb5aec0aadc2cfe9b774bd19c2454c3dd3365a9e
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:57:29 +0300

    updated authenticator rep[o

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,1 +1,19 @@
-﻿// 
+﻿import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pesa_planner/data/models/user_model.dart';
+
+class AuthRepository {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Stream<AppUser> get user {
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      return firebaseUser == null
+          ? AppUser(uid: '', displayName: 'Guest')
+          : AppUser(
+              uid: firebaseUser.uid,
+              email: firebaseUser.email,
+              phone: firebaseUser.phoneNumber,
+              displayName: firebaseUser.displayName ?? 'User',
+            );
+    });
+  }
+}
