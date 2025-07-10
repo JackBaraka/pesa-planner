a3c6c06efb7d1945adad61cb4823ae34b0116a8c
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:52:42 +0300

    updTEd firestore service

--- lib/data/datasources/remote/firestore_service.dart
+++ lib/data/datasources/remote/firestore_service.dart
@@ -1,1 +1,13 @@
-﻿// 
+﻿import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> addExpense(String userId, Map<String, dynamic> expense) {
+    return _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('expenses')
+        .add(expense);
+  }
+}
