57c546b7da10329e7171d995bd770237c37937da
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:29:54 +0300

    updated app colorrs

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,1 +1,9 @@
-﻿// 
+﻿import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color kenyaGreen = Color(0xFF006600);
+  static const Color kenyaRed = Color(0xFFCE1126);
+  static const Color kenyaBlack = Color(0xFF000000);
+  static const Color kenyaWhite = Color(0xFFFFFFFF);
+  static const Color kenyaGold = Color(0xFFF0A800);
+}
