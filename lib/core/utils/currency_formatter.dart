5214de9edcf8a9873cf7d8a0f60c9ce86f61b92d
JackBaraka <jack.baraka15894@gmail.com>
2025-07-10 16:37:43 +0300

    updated dependencies and updated currency formatter

--- lib/core/utils/currency_formatter.dart
+++ lib/core/utils/currency_formatter.dart
@@ -1,1 +1,9 @@
-﻿// 
+﻿import 'package:intl/intl.dart';
+
+String formatKSH(double amount) {
+  return NumberFormat.currency(
+    locale: 'sw_KE',
+    symbol: 'KSh ',
+    decimalDigits: 2,
+  ).format(amount);
+}
