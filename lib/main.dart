3b9e8c6e22702cace4a185a46d32ef710dfbdc9a
JackBaraka <jack.baraka15894@gmail.com>
2025-07-11 11:23:23 +0300

    updATED MAIN

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 ﻿import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:pesa_planner/app_widget.dart';
+import 'package:pesa_planner/services/auth_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const AppWidget());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<AuthService>(create: (_) => AuthService()),
+        // Add other providers here if needed
+      ],
+      child: const AppWidget(),
+    ),
+  );
 }
