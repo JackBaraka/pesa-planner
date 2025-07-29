160f5f6c98ba54df8648d6f6919749b25bf89389
JackBaraka <jack.baraka15894@gmail.com>
2025-07-29 05:23:45 +0300

    updated app router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,11 +1,25 @@
 ﻿import 'package:flutter/material.dart';
-// Make sure that the LoginScreen class is defined as 'class LoginScreen extends StatelessWidget' and exported in the imported file.
+import 'package:pesa_planner/features/auth/presentation/screens/login_screen.dart';
+import 'package:pesa_planner/features/auth/presentation/screens/phone_verify_screen.dart';
+import 'package:pesa_planner/features/budget/presentation/screens/budget_creation_screen.dart';
+import 'package:pesa_planner/features/budget/presentation/screens/budget_list_screen.dart';
+import 'package:pesa_planner/features/dashboard/home_screen.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case '/login':
         return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case '/register':
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+      case '/phone-verify':
+        return MaterialPageRoute(builder: (_) => const PhoneVerifyScreen());
+      case '/budgets':
+        return MaterialPageRoute(builder: (_) => const BudgetListScreen());
+      case '/create-budget':
+        return MaterialPageRoute(builder: (_) => const BudgetCreationScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
@@ -16,11 +30,6 @@ class AppRouter {
   }
 }
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Login Screen')));
-  }
+class RegisterScreen {
+  const RegisterScreen();
 }
