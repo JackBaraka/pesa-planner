5601903c4976e11e11fa8260f6352096215f47e5
JackBaraka <jack.baraka15894@gmail.com>
2025-08-05 02:56:40 +0300

    debugged kiasi

--- lib/features/budget/presentation/screens/budget_list_screen.dart
+++ lib/features/budget/presentation/screens/budget_list_screen.dart
@@ -6,8 +6,6 @@ import 'package:pesa_planner/features/budget/presentation/screens/budget_creatio
 import 'package:pesa_planner/services/auth_service.dart';
 import 'package:pesa_planner/services/database_service.dart';
 import 'package:provider/provider.dart';
-import 'package:pesa_planner/core/routes/app_router.dart';
-import 'package:pesa_planner/core/theme/app_theme.dart';
 
 class BudgetListScreen extends StatelessWidget {
   const BudgetListScreen({super.key});
@@ -17,7 +15,7 @@ class BudgetListScreen extends StatelessWidget {
     final authService = Provider.of<AuthService>(context);
 
     // Show loading while auth is initializing
-    if (!authService.isInitialized) {
+    if (authService.isInitialized == null || !authService.isInitialized!) {
       return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
 
