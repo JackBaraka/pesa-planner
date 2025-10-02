de506bfafccb2c4e15aef3d34ed9efd59e931f27
JackBaraka <jack.baraka15894@gmail.com>
2025-10-02 04:26:57 +0300

    updated dependencies

--- lib/features/reports/presentation/screens/report_generation_screen.dart
+++ lib/features/reports/presentation/screens/report_generation_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
 import 'package:pesa_planner/core/theme/app_colors.dart';
 import 'package:pesa_planner/services/pdf_service.dart';
